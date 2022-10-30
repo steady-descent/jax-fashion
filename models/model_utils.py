cfd48a55163ac466cd30581ccc7f87630a3d4960
Anton Abilov <aabilov@dataminr.com>
2022-10-30 19:54:56 -0400

    Refactored baseline_mlp (achieves 87% on Test after 8 epochs) - Moved most funcs from model_utils into the model code

--- models/model_utils.py
+++ models/model_utils.py
@@ -1,10 +1,6 @@
 import jax.numpy as jnp
-from jax import grad, jit, vmap
-from jax.scipy.special import logsumexp
-
-
-def relu(x):
-    return jnp.maximum(0, x)
+from jax import jit
+import jax
 
 
 def one_hot(x, k, dtype=jnp.float32):
@@ -12,37 +8,6 @@ def one_hot(x, k, dtype=jnp.float32):
     return jnp.array(x[:, None] == jnp.arange(k), dtype)
 
 
-def accuracy(params, images, targets):
-    target_class = jnp.argmax(targets, axis=1)
-    predicted_class = jnp.argmax(batched_predict(params, images), axis=1)
-    return jnp.mean(predicted_class == target_class)
-
-
-def loss(params, images, targets):
-    preds = batched_predict(params, images)
-    return -jnp.mean(preds * targets)
-
-
 @jit
-def update(params, step_size, x, y):
-    grads = grad(loss)(params, x, y)
-    return [
-        (w - step_size * dw, b - step_size * db)
-        for (w, b), (dw, db) in zip(params, grads)
-    ]
-
-
-def predict(params, image):
-    # per-example predictions
-    activations = image
-    for w, b in params[:-1]:
-        outputs = jnp.dot(w, activations) + b
-        activations = relu(outputs)
-
-    final_w, final_b = params[-1]
-    logits = jnp.dot(final_w, activations) + final_b
-    return logits - logsumexp(logits)
-
-
-# This works on single examples
-batched_predict = vmap(predict, in_axes=(None, 0))
+def update(params, step_size, grads):
+    return jax.tree_map(lambda param, g: param - g * step_size, params, grads)
